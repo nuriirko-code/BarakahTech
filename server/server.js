43e4247746473b26d456c6a05310cb788af12bcc
Nuri <nuriirko@gmail.com>
2026-09-25 04:29:36 -0700

    cors added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 require("dotenv").config()
 const connectDB = require("./config/db")
 const authRoutes = require("./routes/authRoutes")
@@ -7,6 +8,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // Middleware
+app.use(cors({ origin: "http://localhost:5173" }))
 app.use(express.json())
 
 // Routes
